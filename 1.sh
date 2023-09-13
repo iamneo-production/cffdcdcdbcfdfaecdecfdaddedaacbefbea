d7683c97eb5447c29c10725a8e40d4e3d0852d23
Iamneo <techadmin@iamneo.ai>
2023-09-13 11:36:20 +0000

    Backup Commit

--- 1.sh
+++ 1.sh
@@ -1,12 +1,12 @@
 d="/home/coder/myfolder"
-if[ -d "$d" ];then
-mkdir -p "$d"
+if[ -d "$d" ];  then
 echo "Directory '$d' created"
 else
+mkdir -p "$d"
 echo "Directory '$d' already exist"
 fi
 f="$d/my.txt"
-if[ ! -e "$f" ];then
+if[ ! -e "$f" ];  then
 touch "$f"
 echo "File '$f' created"
 else
